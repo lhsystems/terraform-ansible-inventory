5381ba0f718e9230f3b946a5b40f7076201633d0
U751582 <peter.czegledi@lhsystems.com>
2021-12-03 14:59:16 +0100

    v1.1.0rc: First version with state pull from TFE

---
+++ src/lib/get_state.js
@@ -0,0 +1,27 @@
+// Import modules and files
+'use strict'
+const core = require('@actions/core')
+const tfe = require('./tfe')
+
+async function pullStateFile(source) {
+    switch (source) {
+      case 'tfe': {
+        const organization = await core.getInput('organization')
+        const workspace = await core.getInput('workspace')
+        const apiToken = await core.getInput('api-token')
+        const jsonState = await tfe.tfePull(apiToken, organization, workspace)
+        return jsonState
+        break;
+      }
+      default: {
+        // Set the action to failed if the state source is not supported
+        core.setFailed(`Unsupported state source has been detected: ${source}`);        
+      }
+
+    }
+}
+
+// Exporting the required modules
+module.exports = {
+    pullStateFile
+}
---
+++ src/lib/tfe.js
@@ -0,0 +1,30 @@
+// Import axios for API call and fs for creating a file
+'use strict'
+const axios = require('axios');
+
+// Get the data from the specified URL
+async function getDataFromURL(dataUrl, instance) {
+    const response = await instance.get(dataUrl)
+    return response.data
+}
+
+// Get the data from the workspace (using async await, instead of nested .then)
+async function tfePull(apiToken, organization, workspace) {
+    // Create the axios config for the request
+    const instance = axios.create({
+      baseURL: 'https://app.terraform.io',
+      timeout: 5000,
+      headers: {'Authorization': 'Bearer '+ apiToken}
+    });
+    
+    const tfeUrl = `https://app.terraform.io/api/v2/organizations/${organization}/workspaces/${workspace}`
+    const workspaceData = await getDataFromURL(tfeUrl, instance)
+    const currentState = await getDataFromURL(workspaceData.data.relationships['current-state-version'].links.related, instance)
+    const stateFile = await getDataFromURL(currentState.data.attributes['hosted-state-download-url'], instance)
+    return stateFile
+}
+
+// Exporting the required modules
+module.exports = {
+    tfePull
+}
